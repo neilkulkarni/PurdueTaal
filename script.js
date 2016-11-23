ea615ce10e160d1890701c12a6717828a766a459
Neil Kulkarni <neil_kulkarni@icloud.com>
2016-11-23 02:08:33 -0800

    Fixes to mobile menu + Responsiveness
    Day 3

--- script.js
+++ script.js
@@ -22,3 +22,39 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+function showContent() {
+    document.getElementById("mobileOverlay").classList.toggle("show");
+    document.getElementById("innerWrapper").classList.toggle("hide");
+    document.getElementById("menuToggle").classList.toggle("change");
+}
+
+function myFunction(x) {
+    document.getElementById("mobileOverlay").classList.toggle("show");
+    document.getElementById("innerWrapper").classList.toggle("hide");
+    x.classList.toggle("change");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.menuToggle')) {
+
+    var dropdowns = document.getElementById("mobileOverlay");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+
+    var wrappers = document.getElementById("innerWrapper");
+    var i;
+    for (i = 0; i < wrappers.length; i++) {
+      var openWrapper = wrappers[i];
+      if (openWrapper.classList.contains('hide')) {
+        openWrapper.classList.remove('hide');
+      }
+    }
+  }
+}
